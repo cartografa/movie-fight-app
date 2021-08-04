cb3fe6a54925b67f8bf4d47ee883d1d247421423
Carolina S. M <carostmaria@gmail.com>
2021-08-03 23:39:10 -0300

    Continue working on reusability

--- js/autocomplete.js
+++ js/autocomplete.js
@@ -1,7 +1,13 @@
-// pass a config object
-const createAutoComplete = ({ root }) => {
+const createAutoComplete = ({
+    // destructuring the config object:
+    root, 
+    renderOption, 
+    onOptionSelect, 
+    inputValue,
+    fetchData 
+}) => {
     root.innerHTML = `
-        <label><b>Search for a Movie</b></label>
+        <label><b>Search</b></label>
         <input class="input" />
         <div class="dropdown">
             <div class="dropdown-menu">
@@ -15,12 +21,11 @@ const createAutoComplete = ({ root }) => {
     const dropdown = root.querySelector('.dropdown');
     const resultsWrapper = root.querySelector('.results');
 
-
     const onInput = async event => {
-        const movies = await fetchData(event.target.value);
+        const items = await fetchData(event.target.value);
 
         // Si no hay resultados, se cierra el dropdown.
-        if (!movies.length) {
+        if (!items.length) {
             dropdown.classList.remove('is-active');
             return;
         }
@@ -28,22 +33,15 @@ const createAutoComplete = ({ root }) => {
         resultsWrapper.innerHTML = '';
         dropdown.classList.add('is-active');
 
-        for (let movie of movies) {
+        for (let item of items) {
             const option = document.createElement('a');
-            const imgSrc = movie.Poster === 'N/A' ? '' : movie.Poster;
-
 
             option.classList.add('dropdown-item');
-            option.innerHTML = `
-                <img src="${imgSrc}"/>
-                ${movie.Title}
-            `;
-            // CLICK EVENT FOR CHOOSING A MOVIE
+            option.innerHTML = renderOption(item);
             option.addEventListener('click', () => {
                 dropdown.classList.remove('is-active');
-                input.value = movie.Title;
-
-                onMovieSelect(movie);
+                input.value = inputValue(item);
+                onOptionSelect(item);
             });
 
             resultsWrapper.appendChild(option);
--- js/index.js
+++ js/index.js
@@ -1,26 +1,33 @@
-const fetchData = async (searchTerm) => {
-    const response = await axios.get('http://www.omdbapi.com/', {
-        params: {
-            apikey: '25e170d',
-            s: searchTerm
+createAutoComplete({
+    root: document.querySelector('.autocomplete'),
+    renderOption(movie) {
+        const imgSrc = movie.Poster === 'N/A' ? '' : movie.Poster;
+        return `
+        <img src="${imgSrc}"/>
+        ${movie.Title} (${movie.Year})
+        `;
+    },
+    onOptionSelect(movie) {
+        onMovieSelect(movie);
+    },
+    inputValue(movie) {
+        return movie.Title;
+    },
+    async fetchData(searchTerm) {
+        const response = await axios.get('http://www.omdbapi.com/', {
+            params: {
+                apikey: '25e170d',
+                s: searchTerm
+            }
+        });
+    
+        if (response.data.Error) {
+            return [];
         }
-    });
-
-    if (response.data.Error) {
-        return [];
+        
+        return response.data.Search;
     }
     
-    return response.data.Search;
-};
-
-createAutoComplete({
-    root: document.querySelector('.autocomplete')
-});
-createAutoComplete({
-    root: document.querySelector('.autocomplete-two')
-});
-createAutoComplete({
-    root: document.querySelector('.autocomplete-three')
 });
 
 // FOLLOW-UP REQUEST FUNCTION WITH MOVIE DATA
